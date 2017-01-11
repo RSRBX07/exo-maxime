a6672fbdeedaeded3b58e2806ea965707add36b8
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-11 16:37:34 +0100

    dice cheat

--- dice.rb
+++ dice.rb
@@ -1,9 +1,20 @@
 #maxime
-def roll
-  dice = [1,2,3,4,5,6]
+def roll cheat
+  if cheat == 0
+    dice = [1,2,3,4,5,6]
+  else
+    dice = [cheat]
+  end
   nbr = dice.size
   result = rand nbr
   dice[result]
 end
 
-puts roll
+print "lancer un dé : "
+input = gets.to_i
+if input == 0
+  incheat = 0
+else
+  incheat = input
+end
+puts roll incheat
