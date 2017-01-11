23ac9656470445f1b183979b8e15dbcfa67ecff6
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-11 16:03:15 +0100

    def roll

--- dice.rb
+++ dice.rb
@@ -1,6 +1,9 @@
-dice = [1,2,3,4,5,6]
-nbr = dice.size
-#puts dice[rand nbr]
-result = rand nbr
-puts dice[result]
- 
+def roll
+  dice = [1,2,3,4,5,6]
+  nbr = dice.size
+  #puts dice[rand nbr]
+  result = rand nbr
+  puts dice[result]
+end
+
+roll
