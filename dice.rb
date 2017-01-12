aab8b01169952839c80e93c526d3e9179095b5c8
Pierre Merlin <pierrem@apido.org>
2017-01-12 09:43:36 +0100

    refact with return keyword use

--- dice.rb
+++ dice.rb
@@ -1,13 +1,14 @@
 #maxime
 def roll (cheat = nil)
+  
   if cheat
     return cheat
-  else
-    dice = [1,2,3,4,5,6]
-    nbr = dice.size
-    result = rand nbr
-    dice[result]
   end
+  
+  dice = [1,2,3,4,5,6]
+  index = rand 6
+  dice[index]
+  
 end
 
 puts roll
