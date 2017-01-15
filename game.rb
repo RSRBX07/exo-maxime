c1792e93d9a720c75b9053dda8a988d3dd09f9c1
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-15 04:12:23 +0100

    patch game, os detect, methode doc
    + Lisibilité.

--- game.rb
+++ game.rb
@@ -1,3 +1,49 @@
+def os
+  if Gem::Platform.local.os == "linux"
+    system("clear")
+  else
+    system("cls")
+  end
+end
+
+def doc
+  puts""
+  puts"Sur la banquise, 4 petits animaux tentent de regagner leur igloo. "
+  puts"Attention car le pont de glace menace de s’écrouler"
+  puts"Un jeu qui se joue en coopération pour apprendre à jouer "
+  puts"et à gagner tous ensemble."
+  puts""
+  puts"Contenu :"
+  puts" 4 animaux, 3 plateaux de jeu (1 plateau de départ « pêche », "
+  puts" 1 pont de glace, 1 plateau d’arrivée « igloo »),"
+  puts" 6 piliers en bois (piliers du pont), 1 dé "
+  puts""
+  puts"Préparation :"
+  puts" Les 3 plateaux sont placés au centre de la table : "
+  puts" le pont entre les deux plateaux "
+  puts" banquise est posé sur les 6 piliers en bois. "
+  puts" Les 4 animaux sont placés sur le plateau "
+  puts" de départ à côté du trou de pêche."
+  puts""
+  puts"Déroulement du jeu : "
+  puts" À chaque tour de jeu, les joueurs peuvent avancer l’animal de leur choix. "
+  puts" On joue dans le sens des aiguilles d’une montre. "
+  puts" Le plus jeune joueur lance le dé."
+  puts""
+  puts"face pont :  le joueur peut avancer l’animal de son choix de la banquise pêche vers le pont"
+  puts""
+  puts"face igloo : le joueur peut avancer l’animal de son choix du pont vers la banquise igloo"
+  puts""
+  puts"face glaçon : la banquise fond … il faut ôter un des piliers du pont"
+  puts""
+  puts"Qui gagne ?"
+  puts"Les joueurs gagnent ou perdent ensemble."
+  puts"Les joueurs gagnent s’ils ont amené tous les animaux sur la banquise igloo avant"
+  puts"que le pont ne s’écroule."
+  puts"En revanche, si le pont s’écroule avant que tous les animaux soient sur la banquise"
+  puts"igloo, tous les joueurs ont perdu la partie."
+end
+
 def dice
   faces = ["pont", "igloo", "glacon"]
   faces.shuffle[0]
@@ -30,13 +76,6 @@ def game
   card_and_animals = {"pingouin" => "peche","lapin" => "peche","ours" => "peche","loup" => "peche"}
   while health_point_pont != 0 
     result_of_dice = dice
-    if health_point_pont == 0
-      system("cls")
-      puts "========================================="
-      puts "|            Vous avez perdu            |"
-      puts "========================================="
-      menu
-    end
     final = 1
     card_and_animals.each_value do |valeur|
       if valeur == "pont" || valeur == "peche"
@@ -44,10 +83,9 @@ def game
       end
     end
     if final == 1 
-      system("cls")
-      puts "========================================="
-      puts "|            Vous avez gagné            |"
-      puts "========================================="
+      os
+      indication = "       Vous avez gagné         "
+      status_message indication
       menu
     end
     if result_of_dice == "pont"
@@ -112,6 +150,12 @@ def game
       health_point_pont -= 1
       puts "Il reste #{health_point_pont} PV au pont."
     end
+    if health_point_pont == 0
+      os
+      indication = "       Vous avez perdu         "
+      status_message indication
+      menu
+    end
   end
 end
 
@@ -119,53 +163,18 @@ def menu
   play = "j"
   help = "r"
   quit = "q"
-  puts "========================================="
-  puts "|      Bienvenue le jeux de l'igloo     |"
-  puts "========================================="
+  indication = "  Bienvenue le jeux de l'igloo "
+  status_message indication
   print "jouer (j) règle du jeu (r) quitter (q): "
   reponse = gets.chomp.downcase.to_sym
   if reponse != play.to_sym && reponse != help.to_sym && reponse != quit.to_sym
-    system("cls")
+    os
     menu
   elsif reponse == play.to_sym
     game
   elsif reponse == help.to_sym
-    system("cls")
-    puts""
-    puts"Sur la banquise, 4 petits animaux tentent de regagner leur igloo. "
-    puts"Attention car le pont de glace menace de s’écrouler"
-    puts"Un jeu qui se joue en coopération pour apprendre à jouer "
-    puts"et à gagner tous ensemble."
-    puts""
-    puts"Contenu :"
-    puts" 4 animaux, 3 plateaux de jeu (1 plateau de départ « pêche », "
-    puts" 1 pont de glace, 1 plateau d’arrivée « igloo »),"
-    puts" 6 piliers en bois (piliers du pont), 1 dé "
-    puts""
-    puts"Préparation :"
-    puts" Les 3 plateaux sont placés au centre de la table : "
-    puts" le pont entre les deux plateaux "
-    puts" banquise est posé sur les 6 piliers en bois. "
-    puts" Les 4 animaux sont placés sur le plateau "
-    puts" de départ à côté du trou de pêche."
-    puts""
-    puts"Déroulement du jeu : "
-    puts" À chaque tour de jeu, les joueurs peuvent avancer l’animal de leur choix. "
-    puts" On joue dans le sens des aiguilles d’une montre. "
-    puts" Le plus jeune joueur lance le dé."
-    puts""
-    puts"face pont :  le joueur peut avancer l’animal de son choix de la banquise pêche vers le pont"
-    puts""
-    puts"face igloo : le joueur peut avancer l’animal de son choix du pont vers la banquise igloo"
-    puts""
-    puts"face glaçon : la banquise fond … il faut ôter un des piliers du pont"
-    puts""
-    puts"Qui gagne ?"
-    puts"Les joueurs gagnent ou perdent ensemble."
-    puts"Les joueurs gagnent s’ils ont amené tous les animaux sur la banquise igloo avant"
-    puts"que le pont ne s’écroule."
-    puts"En revanche, si le pont s’écroule avant que tous les animaux soient sur la banquise"
-    puts"igloo, tous les joueurs ont perdu la partie."
+    os
+    doc
     menu
   elsif reponse == quit.to_sym
     puts "Merci"
