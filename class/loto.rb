b0b0c07d3fad5a46d25cb3734a9c3fac66bfe968
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-16 14:06:17 +0100

    patch

--- class/loto.rb
+++ class/loto.rb
@@ -1,42 +1,46 @@
 class Loto
 
-def new_draw
-  number = (1..45).to_a
-  number.sample(5).sort
-end
-
-def create_a_grid
-  result_game = []
-  while result_game.size <= 4
-    print "jouez un chiffre entre 1 et 45 : "
-    input_number = gets.to_i
-    if input_number < 1 || input_number > 45
-      puts "Jouez un chiffre valide entre 1 et 45."
-    elsif result_game.include?(input_number)
-      puts "Vous avez déjà joué le #{input_number}, merci de rejouer un autre chiffre."
-    else
-      result_game.push(input_number)
+  def create_a_grid
+    result_game = []
+    while result_game.size <= 4
+      print "jouez un chiffre entre 1 et 45 : "
+      input_number = gets.to_i
+      if input_number < 1 || input_number > 45
+        puts "Jouez un chiffre valide entre 1 et 45."
+      elsif result_game.include?(input_number)
+        puts "Vous avez déjà joué le #{input_number}, merci de rejouer un autre chiffre."
+      else
+        result_game.push(input_number)
+      end
     end
+    puts "Votre bulletin est enregistré : #{result_game.sort!}"
+    grid_validation result_game
   end
-  puts "Votre bulletin est enregistré : #{result_game.sort!}"
-  result_game
-end
 
-def check_result draw,result_game
-  ticket_control = 5
-  last_result = 1
+  def grid_validation grid
+    @palyer ||= []
+    @palyer << grid
+    puts "#{@palyer}"
+  end
 
-  ticket_control.times {
-    if result_game[ticket_control - 1] != draw[ticket_control - 1]
-    last_result = 0
-    end
+  def check_result result_game
+    @draw = @draw || (1..45).to_a.sample(5).sort
+    ticket_control = 5
+    last_result = 1
+
+    ticket_control.times {
+      if result_game[ticket_control - 1] != @draw[ticket_control - 1]
+        last_result = 0
+      end
     }
 
-  if last_result == 1
-    puts "Vous avez gagné !"
-  else
-    puts "Vous avez perdu."
+    if last_result == 1
+      puts "draw = #{@draw}"
+      puts "Vous avez gagné !"
+    else
+      puts "draw = #{@draw}"
+      puts "Vous avez perdu."
+    end
   end
-end
 
 end
--- class/test.rb
+++ class/test.rb
@@ -2,6 +2,7 @@ require './loto.rb'
 
 my_loto = Loto.new
 
-draw = my_loto.new_draw
-result = my_loto.create_a_grid
-my_loto.check_result(draw,result)
+# draw = my_loto.new_draw
+
+2.times {my_loto.create_a_grid}
+#grids.each {|grid| my_loto.check_result(grid)}
