31ed5b8978cd20f681e4cb7325257baa2ec481d9
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-16 09:46:34 +0100

    class Dice

--- dice.rb
+++ dice.rb
@@ -1,11 +1,10 @@
 #maxime
-def roll (cheat = nil)
-  
-  return cheat if cheat
-   
-  dice = [1,2,3,4,5,6]
-  index = rand 6
-  dice[index]
+def roll
+  faces = ["pont", "igloo", "glacon"]
+  faces.sample
+end
+
+class Dice
   
 end
 
