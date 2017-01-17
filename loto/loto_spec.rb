ffb60fe1ac25fced9c1e25555f5752abfbb6bd43
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-17 15:51:41 +0100

    patch 42

---
+++ loto/loto_spec.rb
@@ -0,0 +1,27 @@
+require './loto/loto.rb'
+my_loto = Loto.new
+# # draw = my_loto.new_draw
+2.times {
+  my_loto.grid_validation Loto.create_a_grid
+  puts "@players = #{my_loto.players.inspect}"
+  puts "@draw    = #{my_loto.draw.inspect}"
+}
+
+
+if my_loto.check_result
+  puts "Vous avez gagné"
+else
+  puts "Vous avez perdu"
+end
+
+puts "@players = #{my_loto.players.inspect}"
+puts "@draw    = #{my_loto.draw.inspect}"
+
+
+if my_loto.grid_validation Loto.create_a_grid 
+else
+  puts "tirage déjà fait, reéssayer un autre jour."
+end
+
+puts "@players = #{my_loto.players.inspect}"
+puts "@draw    = #{my_loto.draw.inspect}"
--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -1,27 +1,5 @@
-require './loto.rb'
-my_loto = Loto.new
-# # draw = my_loto.new_draw
-2.times {
-  my_loto.grid_validation Loto.create_a_grid
-  puts "@players = #{my_loto.players.inspect}"
-  puts "@draw    = #{my_loto.draw.inspect}"
-}
+require './lib/Loto.rb'
 
+RSpec.describe Loto do
 
-if my_loto.check_result
-  puts "Vous avez gagné"
-else
-  puts "Vous avez perdu"
-end
-
-puts "@players = #{my_loto.players.inspect}"
-puts "@draw    = #{my_loto.draw.inspect}"
-
-
-if my_loto.grid_validation Loto.create_a_grid 
-else
-  puts "tirage déjà fait, reéssayer un autre jour."
-end
-
-puts "@players = #{my_loto.players.inspect}"
-puts "@draw    = #{my_loto.draw.inspect}"
+  end
