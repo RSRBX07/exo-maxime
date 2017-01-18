d663ab432dca6968a00dde7066cbb6e3fd3c1e63
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-18 16:15:50 +0100

    add self.new

--- vehicle/plane.rb
+++ vehicle/plane.rb
@@ -1,6 +1,10 @@
 require './vehicle/vehicle.rb'
 class Plane < Vehicle
     #attr_reader :flight
+    def self.new
+        puts "Im creating a new Vehicle"
+        super
+    end
 
     def initialize
         super
@@ -21,6 +25,7 @@ class Plane < Vehicle
 end
 
 p = Plane.new
+puts p.class.ancestors.inspect
 puts p.position
 #Tours = latitude: 47.394144000000004,longitude: 0.6848399999999288
 p.move(-3.2985609,-2.493007)
