d9a84b9c59a5c7a77849e042d5de96ea43a5acea
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-19 22:04:16 +0100

    work in progress

--- rar.rb
+++ rar.rb
@@ -1,4 +1,6 @@
 #!/usr/share/rvm/rubies/ruby-2.4.0/bin/ruby
+#require 'rar'
+
 number_name = (8..12).to_a.sample
 grid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 name_rar_file = ""
@@ -9,6 +11,10 @@ number_name.times {
   rd = rand(grid.size)
   name_rar_file = name_rar_file << grid[rd]
 }
+
+
+#archive = RAR::Archive.new name_rar_file
+#archive.add_file "#{ARGV[0]}"
 
 system("rar a -hp#{password} #{name_rar_file} -ed #{ARGV[0]}")
 
