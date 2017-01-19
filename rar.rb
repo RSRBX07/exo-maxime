bd3ed671ca4429232a12ca9fd3d163e2e6735f94
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-19 20:35:10 +0100

    fonctionel rar file

--- rar.rb
+++ rar.rb
@@ -1,8 +1,8 @@
-
+#!/usr/share/rvm/rubies/ruby-2.4.0/bin/ruby
 number_name = (8..12).to_a.sample
 grid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 name_rar_file = ""
-password = ""
+password = "your_password"
 
 
 number_name.times {
@@ -10,6 +10,7 @@ number_name.times {
   name_rar_file = name_rar_file << grid[rd]
 }
 
-system("rar a -hp#{password} #{name_rar_file} /home/vrok/Téléchargements/captvty-2.5.3-autoextract.exe")
+system("rar a -hp#{password} #{name_rar_file} -ed #{ARGV[0]}")
+
 
 
