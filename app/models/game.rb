010565e22d48dea967639aabb14fd2f726010e1b
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-23 16:00:16 +0100

    return players if players <= 0

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,19 @@
 class Game < ApplicationRecord
+
+
+  def add_player
+    @players += 1  
+  end
+
+  def del_player
+    return @players if @players <= 0
+    @players -= 1
+  end
+
+private
+
+  def initialize
+    @players = 0
+  end
+
 end
