95ec557578721be55b82d9532c803e0312c9a360
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-24 16:49:09 +0100

    work in progress

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   #@players = Games.find(:all, :limit => params[:players])
 
-  
+
   
   def add_player
     @players += 1  
@@ -11,6 +11,18 @@ class Game < ApplicationRecord
     raise RuntimeError.new "no more player to be removed" if @players <= 0
     @players -= 1
   end
+
+  def draw_t
+    grid = "1234567890"
+    name_rar_file = ""
+
+    4.times {
+      rd = rand(grid.size)
+      name_rar_file = name_rar_file << grid[rd]
+    }
+    return name_rar_file
+  end
+  #edit_game_path(@game.draw,'[1,2,3,4]')
 
 private
 
